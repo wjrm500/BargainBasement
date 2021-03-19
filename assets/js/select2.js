a36063cb930e462591a495beb24fcbf442f5e7fa
William May <wjrm500@gmail.com>
2021-03-19 09:17:23 +0000

    Fix to previous commit to make sure Select2 dropdowns in Admin are only populated with data from their own data attributes

--- assets/js/select2.js
+++ assets/js/select2.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    $('select').select2();
-    var value = $('select').data('value');
-    var data;
-    if (typeof value === 'number') {
-        data = value;
-    } else {
-        data = $('select').data('value').split(', ');
-    }
-    $('select').val(data).trigger('change');
+    $('select').each(function() {
+        $(this).select2();
+        var value = $(this).data('value');
+        var data;
+        if (typeof value === 'number') {
+            data = value;
+        } else {
+            data = $(this).data('value').split(', ');
+        }
+        $(this).val(data).trigger('change');
+    });
 });
